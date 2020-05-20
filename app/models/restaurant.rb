08f669f0433eaf07856bcfb3ae1fe3da077f1457
lea sordoillet <leasordoillet@gmail.com>
2020-05-20 12:34:19 +0200

    validaions added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  # CATEGORY = w %(chinese italian japanese french belgian)
+  CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
