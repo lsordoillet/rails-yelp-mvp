2dcd2773a773e71750414ffa6832c3f9e1470956
lea sordoillet <leasordoillet@gmail.com>
2020-05-20 14:53:17 +0200

    added routing, index, show and new view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,36 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
+
+  # GET /restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET "restaurants/new"
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # POST "restaurants"
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  # GET "restaurants/38"
+  def show
+    @reviews = Review.new
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = review.new(review_params)
+    @review.save
+
+    redirect_to restaurant_path(@review)
+  end
+
+  private
+
+  def review_params
+    params.require(:restaurant).permit(:content, :rating)
+  end
+
+
 end
